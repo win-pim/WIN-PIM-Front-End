556077558efb65cb7757ebc156beb355c4aa3f2f
bruicejonson <bruicejonson@gmail.com>
2019-08-15 11:51:03 -0400

    Channel

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./channel.component.css']
 })
 export class ChannelComponent implements OnInit {
-
+  showFiller = false;
   constructor() { }
 
   ngOnInit() {
